a38b70ecb6011377493506c43b2ff2645a201b94
Jason Waldrip <jason@waldrip.net>
2012-10-18 23:52:50 -0600

    Proxy is agnostic with ENV vars

--- proxy.rb
+++ proxy.rb
@@ -4,39 +4,49 @@ require 'faraday'
 require 'eventmachine'
 require 'net/http'
 require 'pp'
-require 'uri'
 
-set :destination, 'http://minecraft.waldrip.net:8123'
+# Set the destination based on an Environment Variable
+set :destination, ENV['DESTINATION']
+
+# Build The Connection
 set :connection, (Faraday::Connection.new(url: settings.destination) do |builder|
   builder.use Faraday::Adapter::EMSynchrony # make http request with eventmachine and synchrony
-  #builder.use Faraday::Response::Yajl       # parse body with yajl
 end)
 
+# All Paths Hit the Proxy
 get '/*' do |path|
-  proxy path
+  proxy(path)
 end
 
 put '/*' do |path|
-  proxy path
+  proxy(path)
 end
 
 patch '/*' do |path|
-  proxy path
+  proxy(path)
 end
 
 options '/*' do |path|
-  proxy path
+  proxy(path)
 end
 
 delete '/*' do |path|
-  proxy path
+  proxy(path)
 end
 
-def proxy path
+# Proxy the response to the destination
+def proxy(path)
+
+  # Determine the Request Method
   verb = request.env['REQUEST_METHOD'].downcase.to_sym
-  full_path = [settings.destination, path].join('/')
-  response = eval "settings.connection.#{verb} '#{full_path}'"
+
+  # Get the Response From the Proxy
+  response = eval "settings.connection.#{verb} '#{path}'"
+
+  # Return the Proxy Response Headers
   headers response.headers
+
+  # Return the Proxy Response Body
   body response.body
 end
 
